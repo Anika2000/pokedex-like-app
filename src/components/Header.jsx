46e46f9860a7c7bbc3419309e9ac45bd034b4fca
Anika2000 <anika.sultana@mail.utoronto.ca>
2024-05-12 16:45:07 -0400

    modified search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import NavLink from './NavLink'
 import { FaSearch } from "react-icons/fa";
+import { useState, useRef, useEffect } from 'react';
 
 const Header = () => {
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (isSearchOpen && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isSearchOpen]);
+
+    const toggleSearch = () => {
+        setIsSearchOpen(!isSearchOpen);
+      };
+
+
   return (
     <div className='shadow-sm sticky top-0 bg-white bg-opacity-30 z-30 p-3'>
       <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -21,8 +38,29 @@ const Header = () => {
         </div>
         
         {/* search icon */}
-        <FaSearch className='cursor-pointer'/>
+        <FaSearch className='cursor-pointer' onClick={toggleSearch}/>
 
+        {isSearchOpen && (<div
+          className={`fixed inset-0 z-40 flex justify-center items-center 
+          transition-opacity duration-300 ease-in-out ${
+            isSearchOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+          }`}
+        >
+          <div className="fixed inset-0 bg-gray-800 bg-opacity-50 backdrop-blur-sm"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 
+          -translate-y-1/2 w-3/4 max-w-md">
+            <form>
+            <input
+                ref={inputRef}
+                type="text"
+                placeholder="Please 'Return' to Search"
+                className="bg-transparent w-full text-3xl 
+                text-white placeholder-gray-400 outline-none focus:outline-none
+                text-left"
+              />
+            </form>
+          </div>
+        </div>)}
       </div>
     </div>
   )
